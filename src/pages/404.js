3d365bb47a2499a1851224bb0f450741db988b36
5p1n0 <lu.586@hotmail.it>
2021-12-15 16:55:26 +0100

    Fix gatsby cloud build

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,12 @@
 import * as React from 'react'
 
 const NotFoundPage = () => {
-
-  
+  return (
+    <div className="bg-dark-gray">
+      <h3>Page not Found</h3>
+      <p>Socc'mel!</p>
+    </div>
+  )
 }
 
 export default NotFoundPage
