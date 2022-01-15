a48fa1e417ab5f8d8b003ac7a0d60e9434f0020d
5p1n0 <lu.586@hotmail.it>
2022-01-15 18:53:52 +0100

    Fix footer background color

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 const Footer = () => {
 
   return (
-    <div className="h-48 grid grid-rows-1 items-center justify-items-center bg-dark-gray-dark">
+    <div className="h-48 grid grid-rows-1 items-center justify-items-center bg-dark-gray-darkest">
       <p className="text-center text-xs mb-0">Designed with Tailwind & Built with Gatsy</p>
     </div>
   )
