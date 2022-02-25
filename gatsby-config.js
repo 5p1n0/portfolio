a3c711be82e6a78ecd9720e0462b8138277e1e86
5p1n0 <lu.586@hotmail.it>
2022-02-25 17:30:36 +0100

    Update gatsby configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ require("dotenv").config({
 })
 
 module.exports = {
-  // to enhance, check (gatsby-starter-elemental github)
   siteMetadata: {
     title: "My Portfolio",
     description: "My AWESOME portfolio",
@@ -16,35 +15,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/`,
-        name: `content`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         path: `${__dirname}/content/assets`,
         name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/featured`,
-        name: `featured`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 756,
-            },
-          },
-        ],
       },
     },
     {
