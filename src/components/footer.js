f67d97ec57c7413c57287e9b16c3f85d3fdabc77
5p1n0 <lu.586@hotmail.it>
2022-03-15 14:23:44 +0100

    Update footer description

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,8 @@ import * as React from 'react'
 const Footer = () => {
 
   return (
-    <div className="h-48 grid grid-rows-1 items-center justify-items-center bg-dark-gray-darkest">
-      <p className="text-center text-xs mb-0">Designed with Tailwind & Built with Gatsy</p>
+    <div className="h-40 grid grid-rows-1 items-center justify-items-center bg-dark-gray-darkest">
+      <p className="text-light-gray text-center text-xs mb-0">Built with Gatsby, Tailwind and 🤍<br/><br/>Check my <a className=' opacity-60 hover:opacity-100 hover:text-vivid-orange' href="https://xd.adobe.com/view/13a9fa0e-16a3-4899-a0cb-da1b3631a2be-46fa/?fullscreen">design prototype</a></p>
     </div>
   )
 
